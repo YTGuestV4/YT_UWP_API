3808a6c02b9912f41bd434198e3272edcfb74bb6
YT_Guest V4 <143789124+YTGuestV4@users.noreply.github.com>
2023-09-01 21:56:51 +0300

    Update Addresses.hpp

--- YT/Addresses.hpp
+++ YT/Addresses.hpp
@@ -32,7 +32,7 @@ namespace Addresses
 {
     // remember v30 = sub_979070(pServiceName, 0);
     using r_luavm_load_t = uintptr_t(__fastcall*)(uintptr_t rl, std::string* source, const char* chunk, int env);
-    r_luavm_load_t r_luavm_load = (r_luavm_load_t)(ASLR(0x7D3660)); // string (oldResult, moduleRef  = ...)
+    r_luavm_load_t r_luavm_load = (r_luavm_load_t)(ASLR(0x954E90)); // string (oldResult, moduleRef  = ...)
 
     using r_taskdefer_t = uintptr_t(__cdecl*)(uintptr_t rl);// [Maximum re-entrancy depth (%i) exceeded calling task.defer] or [defer]
     r_taskdefer_t r_taskdefer = (r_taskdefer_t)(ASLR(0x953DF0)); // push
@@ -45,18 +45,19 @@ namespace Addresses
 
     using get_global_state = uintptr_t(__thiscall*)(int, int*, int*);
     get_global_state get_global_state_2 = (get_global_state)(ASLR(0x8555F0)); // Script Start or %s %s detected as malicious.  %s will not run. // most be in line 825 pseudocode
+
     uint64_t check_PlaceId = aslr_0(0x320F0C0); // Windows10Universal.exe+320F0C0
 
 }
 
+
 namespace Deobfuscation {
     uintptr_t luastate(uintptr_t state)
     {
-   /*   v22 = (168 * sub_8555C0() + this + 276);
-        return *v22 - v22;*/
+        /*   v22 = (168 * sub_8555C0() + this + 276);
+             return *v22 - v22;*/
         auto gay = 168 * 0 + state + 276; // remeber to check + or - or ^
         auto girl = gay - *reinterpret_cast<uintptr_t*>(gay);
         return girl;
     }
 }
-
