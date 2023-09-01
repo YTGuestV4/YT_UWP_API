66c4b4d732d3edf513b8dfc7663a2fa457022db2
YT_Guest V4 <143789124+YTGuestV4@users.noreply.github.com>
2023-09-01 21:57:55 +0300

    Update LuaState.hpp

--- YT/LuaState.hpp
+++ YT/LuaState.hpp
@@ -12,48 +12,49 @@ uintptr_t lua_state_global;
 
 namespace LuaState
 {
-    int get_job_by_name(std::string job_name)
-    {
+    int get_job_by_name(const std::string& job_name) {
         auto job_return = 0;
 
-        auto job_start = *(uintptr_t*)(Addresses::r_getscheduler() + 308);
+        auto job_start = *reinterpret_cast<uintptr_t*>(Addresses::r_getscheduler() + 0x134);
+        auto job_end = *reinterpret_cast<uintptr_t*>(Addresses::r_getscheduler() + 0x138);
 
-        while (job_start != *(uintptr_t*)(Addresses::r_getscheduler() + 312)) // *(this + 312) = 0;
-        {
-            if (*(std::string*)(*(uintptr_t*)(job_start) + 128) == job_name) // updated
-            {
-                job_return = *(uintptr_t*)(job_start);
+        while (job_start != job_end) {
+            auto job_name_ptr = *reinterpret_cast<std::string*>(*reinterpret_cast<uintptr_t*>(job_start) + 0x80);
+
+            if (job_name_ptr == job_name) {
+                job_return = *reinterpret_cast<uintptr_t*>(job_start);
+                break;
             }
+
             job_start += 8;
-        }       
-        if (Debug == true) {
-            std::string job_returnv = "job_return value: " + std::to_string(job_return);
-            Addresses::r_lua_print(0, const_cast<char*>(job_returnv.c_str()));
         }
+
         return job_return;
     }
 
-    int get_script_context()
-    {
-        auto script_context = *(uintptr_t*)(get_job_by_name("WaitingHybridScriptsJob") + 424); // old 304 *(this + 312) = a2;
-        if (Debug == true) {
-            std::string get_script_contextvalue = "script_context value: " + std::to_string(script_context);
-            Addresses::r_lua_print(0, const_cast<char*>(get_script_contextvalue.c_str()));
+    int get_script_context() {
+
+        const std::string job_name = "WaitingHybridScriptsJob";
+
+        uintptr_t job = get_job_by_name(job_name);
+
+        if (job == 0) {
+            Addresses::r_lua_print(0, "oh god not again");
+            return 0;
         }
-        return script_context;
+
+        auto script_context = *reinterpret_cast<uintptr_t*>(job + 0x1A8);
+        return static_cast<int>(script_context);
     }
 
-     int get_robloxstate()
-     {
-         return Deobfuscation::luastate(get_script_context());
-     }
 
-    //int get_robloxstate()
-    //{
-    //    int identity = 0;
-    //    int script = 0;
-    //    uintptr_t script_context_address = get_script_context();
-    //    uintptr_t roblox_state_address = Addresses::get_global_state_2(script_context_address, &identity, &script);
-    //    return roblox_state_address;
-    //}
+    int get_roblox_state()
+    {
+        int identity = 8;
+        int script = 0;
+
+        uintptr_t script_context_address = get_script_context();
+        uintptr_t roblox_state_address = Addresses::get_global_state_2(script_context_address, &identity, &script);
+        return roblox_state_address;
+    }
 }
