e1c2605d339af581ad97118bd9e6c4e3d383dcbb
YT_Guest V4 <143789124+YTGuestV4@users.noreply.github.com>
2023-09-08 06:17:28 +0300

    Update Lua.hpp

--- YT/Lua.hpp
+++ YT/Lua.hpp
@@ -6,7 +6,6 @@
 #include <mutex>
 #include "LuaState.hpp"
 
-
 namespace Lua
 {
     void setlevel(uintptr_t rl, int8_t identity)
@@ -15,11 +14,4 @@ namespace Lua
         int8_t* identityPtr = reinterpret_cast<int8_t*>(address + 24); 
         *identityPtr = identity;
     }
-
-    //void setlevel(uintptr_t rl, const char* newIdentity) {
-    //    uintptr_t address = *(uintptr_t*)(rl + 72);
-    //    int8_t* identityPtr = reinterpret_cast<int8_t*>(address + 24);
-
-    //    strcpy(reinterpret_cast<char*>(identityPtr), newIdentity);
-    //}
 }
