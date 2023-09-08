a50da1b464b31b6f63d381661822346cc4de89b6
YT_Guest V4 <143789124+YTGuestV4@users.noreply.github.com>
2023-09-08 06:12:22 +0300

    Update LuaState.hpp

--- YT/LuaState.hpp
+++ YT/LuaState.hpp
@@ -6,47 +6,31 @@
 #include <mutex>
 #include "Addresses.hpp"
 
-bool Debug = false;
-uintptr_t lua_state_global;
-
-
 namespace LuaState
 {
-    int get_job_by_name(const std::string& job_name) {
+    int get_job_by_name(std::string job_name)
+    {
         auto job_return = 0;
 
-        auto job_start = *reinterpret_cast<uintptr_t*>(Addresses::r_getscheduler() + 0x134);
-        auto job_end = *reinterpret_cast<uintptr_t*>(Addresses::r_getscheduler() + 0x138);
+        auto job_start = *(uintptr_t*)(Addresses::r_getscheduler() + 308);
 
-        while (job_start != job_end) {
-            auto job_name_ptr = *reinterpret_cast<std::string*>(*reinterpret_cast<uintptr_t*>(job_start) + 0x80);
-
-            if (job_name_ptr == job_name) {
-                job_return = *reinterpret_cast<uintptr_t*>(job_start);
-                break;
+        while (job_start != *(uintptr_t*)(Addresses::r_getscheduler() + 312)) // *(this + 316) = 0;
+        {
+            if (*(std::string*)(*(uintptr_t*)(job_start)+128) == job_name)  // 0x80 128
+            {
+                job_return = *(uintptr_t*)(job_start);
             }
-
             job_start += 8;
         }
 
         return job_return;
     }
 
-    int get_script_context() {
-
-        const std::string job_name = "WaitingHybridScriptsJob";
-
-        uintptr_t job = get_job_by_name(job_name);
-
-        if (job == 0) {
-            Addresses::r_lua_print(0, "oh god not again");
-            return 0;
-        }
-
-        auto script_context = *reinterpret_cast<uintptr_t*>(job + 0x1A8);
-        return static_cast<int>(script_context);
+    int get_script_context()
+    {
+        auto script_context = *(uintptr_t*)(get_job_by_name("WaitingHybridScriptsJob") + 424); // 424  0x1A8
+        return script_context;
     }
-
 
     int get_roblox_state()
     {
